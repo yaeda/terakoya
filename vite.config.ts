dfa29e24a2ee9854686de82ebb1b586bd3fcd3c8
Takeshi Yaeda <t.yaeda@gmail.com>
2025-03-15 12:10:28 +0900

    build: set `base` for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  base: "/terakoya/",
 });
