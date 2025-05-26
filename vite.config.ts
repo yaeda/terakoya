ade74854d5d3b87404fc4f370cac1f4af322d499
Takeshi Yaeda <t.yaeda@gmail.com>
2025-05-26 19:08:07 +0900

    build: add vite-plugin-qrcode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { qrcode } from "vite-plugin-qrcode";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), qrcode()],
   base: "/terakoya/",
 });
